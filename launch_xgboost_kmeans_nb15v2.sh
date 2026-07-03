c8a68d936b763fde5449079f96b87c9ee4d24771
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2026-07-03 22:49:17 +0200

    Create launch_xgboost_kmeans_nb15v2.sh

---
+++ launch_xgboost_kmeans_nb15v2.sh
@@ -0,0 +1,9 @@
+make prepare DATA=nb15_v2 NAME=topk_inference_xgboost_kmeans CLASSIFIER=xgboost CLUSTERING=kmeans
+
+make complexity DATA=nb15_v2 NAME=topk_inference_xgboost_kmeans CLASSIFIER=xgboost CLUSTERING=kmeans EXTEND=1
+
+make classify DATA=nb15_v2 NAME=topk_inference_xgboost_kmeans CLASSIFIER=xgboost CLUSTERING=kmeans
+
+make classify-extended DATA=nb15_v2 NAME=topk_inference_xgboost_kmeans CLASSIFIER=xgboost CLUSTERING=kmeans
+
+make topk-inference DATA=nb15_v2 NAME=topk_inference_xgboost_kmeans CLASSIFIER=xgboost CLUSTERING=kmeans
