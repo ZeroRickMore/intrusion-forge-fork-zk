ba36454fd38319fa76286e3142f150e9d8e941f3
ZeroRickMore <riccardo.moretti.gg@gmail.com>
2026-07-03 22:49:12 +0200

    Delete launch_kmeans2.sh

--- launch_kmeans2.sh
+++
@@ -1,9 +0,0 @@
-make prepare DATA=nb15_v2 NAME=topk_inference_kmeans2 CLASSIFIER=random_forest CLUSTERING=kmeans
-
-make complexity DATA=nb15_v2 NAME=topk_inference_kmeans2 CLASSIFIER=random_forest CLUSTERING=kmeans EXTEND=1
-
-make classify DATA=nb15_v2 NAME=topk_inference_kmeans2 CLASSIFIER=random_forest CLUSTERING=kmeans
-
-make classify-extended DATA=nb15_v2 NAME=topk_inference_kmeans2 CLASSIFIER=random_forest CLUSTERING=kmeans
-
-# make topk-inference DATA=nb15_v2 NAME=topk_inference_kmeans2 CLASSIFIER=random_forest CLUSTERING=kmeans
